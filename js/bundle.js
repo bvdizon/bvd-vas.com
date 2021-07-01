7b6c4feea6f0685329f402766722f77065045a66
briandizon <bvdizon@gmail.com>
2021-07-01 09:39:39 +0800

    portfolio - layout update

--- js/bundle.js
+++ js/bundle.js
@@ -1,4 +1,19 @@
 import GLightbox from './vendor/glightbox';
 import './scripts/menu';
 
-const lightboxCrmCms = GLightbox();
+const lightboxCrmCms = GLightbox({
+  touchNavigation: true,
+  loop: true,
+});
+
+const lightboxGraphicsVideo = GLightbox({
+  selector: '.glightboxGV',
+  touchNavigation: true,
+  loop: true,
+});
+
+const lightboxWeb = GLightbox({
+  selector: '.glightboxWeb',
+  touchNavigation: true,
+  loop: true,
+});
