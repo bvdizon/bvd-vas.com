1177f39fa7f4fb33a5a35c4037518257a2a50df5
briandizon <bvdizon@gmail.com>
2021-07-01 16:32:55 +0800

    intersection obs complete

--- js/bundle.js
+++ js/bundle.js
@@ -20,7 +20,21 @@ const lightboxWeb = GLightbox({
 });
 
 // INTERSECTION OBSERVER
-let observer = new IntersectionObserver(callback);
+let observer = new IntersectionObserver(function (entries, observer) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // do this when the element enters the viewport
+      loadElement(entry.target);
+      // stop watching
+      observer.unobserve(entry.target);
+    }
+  });
+});
+
+function loadElement(element) {
+  const src = element.getAttribute('data-src');
+  element.src = src;
+}
 
 // the element that you want to watch
 const element = document.querySelector('iframe#calendly');
